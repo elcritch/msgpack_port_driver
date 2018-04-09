c0beba957a45055bec37775fbdb96f8e04981b00
Jaremy Creechley <creechley@gmail.com>
2018-04-09 13:20:29 -0600

    Initial commit

---
+++ lib/rpclib_port_driver.ex
@@ -0,0 +1,18 @@
+defmodule RpclibPortDriver do
+  @moduledoc """
+  Documentation for RpclibPortDriver.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RpclibPortDriver.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/rpclib_port_driver_test.exs
@@ -0,0 +1,8 @@
+defmodule RpclibPortDriverTest do
+  use ExUnit.Case
+  doctest RpclibPortDriver
+
+  test "greets the world" do
+    assert RpclibPortDriver.hello() == :world
+  end
+end
