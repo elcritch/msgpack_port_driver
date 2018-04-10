f7567eca419ff659c7a33284f60213081e87d274
Jaremy Creechley <creechley@gmail.com>
2018-04-09 22:19:06 -0600

    Documentation updates

--- test/rpclib_port_driver_test.exs
+++ test/rpclib_port_driver_test.exs
@@ -2,7 +2,7 @@ defmodule RpclibPortDriverTest do
   use ExUnit.Case
   doctest RpclibPortDriver
 
-  test "greets the world" do
-    assert RpclibPortDriver.hello() == :world
-  end
+  # test "greets the world" do
+    # assert RpclibPortDriver.hello() == :world
+  # end
 end
