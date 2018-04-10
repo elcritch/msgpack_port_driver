529ba60dc5000ee1cca2e3fa6cba6e6acae81d1e
Jaremy Creechley <creechley@gmail.com>
2018-04-09 21:23:34 -0600

    updating lib -- fix path linking

--- lib/mix/tasks/gen_driver.ex
+++ lib/mix/tasks/gen_driver.ex
@@ -33,7 +33,7 @@ defmodule Mix.Tasks.Rpclib.Gen.Driver do
     dep_path =
       Mix.Project.deps_paths()[:rpclib_port_driver]
       |> Path.relative_to_cwd()
-      |> fn p -> p <> "/src" end.()
+      |> fn p -> p <> "/src/dispatcher/" end.()
 
     dep_path =
       case Path.type(dep_path) do
